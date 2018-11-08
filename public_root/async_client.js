48cbd0f0be1ba798b2e52121d8375907592d401a
crownedsilverfox <byskillcfpro@gmail.com>
2018-11-08 12:04:07 +0700

    Added Progress bar, cosmetics.

--- public_root/async_client.js
+++ public_root/async_client.js
@@ -1,23 +1,28 @@
+let progress;
+let done_blocks = [];
+let blocks;
+
 async function get_part(start, end, url) {
     let headers = new Headers();
     headers.append("Range", "bytes=" + start + "-" + end);
     let request = new Request(url, {headers: headers});
-    let mBlob = new Blob();
     let response = (await fetch(request)).blob().then(function (myBlob) {
         return myBlob;
     });
+    response = await response;
+    done_blocks.push(start);
+    progress = 100*(done_blocks.length/blocks);
+    $('.progress-bar').attr('aria-valuenow', Number(Math.ceil(progress))).text(Number(Math.ceil(progress)) + '%').css('width', Number(Math.ceil(progress))+'%');
     return response;
 }
 
 function get_content(chunks) {
-    let file = document.getElementById('FileSelect');
-    file = file.options[file.selectedIndex].text;
+    let file = $('#FileSelect option:selected');
+    file = file.text();
     let size;
     file = file.split(': ');
     size = Number(file[1]);
     file = file[0];
-    console.log(file);
-    console.log(size);
     let url = "http://localhost:8888/get/" + file;
     let chunk_size = Math.floor(size / chunks);
     let bytes = [];
@@ -27,15 +32,17 @@ function get_content(chunks) {
     if ((bytes[bytes.length - 1]) !== size) {
         bytes[bytes.length - 1] = size+1;
     }
+    blocks = bytes.length;
+    done_blocks = [];
+    progress = 0;
     let promises = [];
     for (let i = 1; i < bytes.length; i++) {
         promises.push(get_part(bytes[i - 1], bytes[i]-1, url));
     }
     let blob;
     Promise.all(promises).then(function (results) {
-        console.log(results);
         blob = new Blob(results);
+        $('.progress-bar').attr('aria-valuenow', 100).text(100 + '%').css('width', 100 + '%');
         download(blob, file);
-        // console.log(blob.size)
     });
 }
--- server.py
+++ server.py
@@ -14,40 +14,26 @@ class App:
         pass
 
 
-class MainHandler(tornado.web.RequestHandler):
+class RangeHandler(tornado.web.RequestHandler):
 
     chunk_len = 1024
     content_size = 0
 
     async def get(self, path, include_body=True):
-        # Assume that path is correct, validation will be handeled elsewhere
         absolute_path = os.path.abspath(path)
-        print(path)
         path = absolute_path
         if absolute_path is None:
             return
 
-        # self.modified = self.get_modified_time()
-        # self.set_headers()
-        #
-        # if self.should_return_304():
-        #     self.set_status(304)
-        #     return
-
         request_range = None
         range_header = self.request.headers.get("Range")
         if range_header:
-            # As per RFC 2616 14.16, if an invalid Range header is specified,
-            # the request will be treated as if the header didn't exist.
             request_range = _parse_request_range(range_header)
         if request_range:
             start, end = request_range
             await self.set_content_size(absolute_path)
             size = self.content_size
             if (start and (start >= size)) or end == 0:
-                # As per RFC 2616 14.35.1, a range is not satisfiable only: if
-                # the first requested byte is equal to or greater than the
-                # content, or when a suffix with length 0 is specified
                 self.set_status(416)  # Range Not Satisfiable
                 self.set_header("Content-Type", "text/plain")
                 self.set_header("Content-Range", "bytes */%s" % (size,))
@@ -55,8 +41,6 @@ class MainHandler(tornado.web.RequestHandler):
             if start and (start < 0):
                 start += size
             if end and (end > size):
-                # Clients sometimes blindly use a large range to limit their
-                # download size; cap the endpoint at the actual file size.
                 end = size
 
             self.set_status(206)  # Partial Content
@@ -82,14 +66,6 @@ class MainHandler(tornado.web.RequestHandler):
             point2 = point2 if point2 <= end else end
             content.append(bytes(bts[point: point2]))
             point = point2
-        # point = start
-        # bts = b'Fuck this Fucking World'
-        # while point < end:
-        #     point2 = point + self.chunk_len
-        #     point2 = point2 if point2 <= end else end
-        #     content.append(bts[point: point2])
-        # print(content)
-        logs.write(str(content))
         return content
     
     async def set_content_size(self, abspath):
@@ -100,7 +76,7 @@ class MainHandler(tornado.web.RequestHandler):
         # print(self.content_size)
 
 
-class Handle(tornado.web.RequestHandler):
+class IndexHandler(tornado.web.RequestHandler):
     def get(self, *args, **kwargs):
         return self.render('templates/base.html')
 
@@ -124,11 +100,10 @@ class Handle(tornado.web.RequestHandler):
 
 if __name__ == "__main__":
     application = tornado.web.Application([
-        (r"/", Handle),
+        (r"/", IndexHandler),
         (r"/static/(.*)", tornado.web.StaticFileHandler, {'path': "public_root"}),
-        (r"/get/(.*)", MainHandler),
+        (r"/get/(.*)", RangeHandler),
     ])
-    logs = open('logs', 'w')
     sock = 8888
     application.listen(sock)
     myIP = socket.gethostbyname(socket.gethostname())
@@ -138,4 +113,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print('*** Stopped server ***')
         tornado.ioloop.IOLoop.current().stop()
-    logs.close()
